4195c3a9acd8f1ce9b7ebcbb680cdba31b9b2f6b
cpalomino1990 <89701110+cpalomino1990@users.noreply.github.com>
2025-04-28 17:20:07 -0500

    Actualizar archivo de entornos: comentar la dirección IP local y mantener la URL de producción activa.

--- src/shared/constants/enviroments.js
+++ src/shared/constants/enviroments.js
@@ -1,5 +1,7 @@
-export const host = "http://192.168.1.151:5502";
+// export const host = "http://192.168.1.151:5502";
 // export const host = "http://127.0.0.1:5502"; // Production
+export const host = "https://peaceful-biscotti-94162e.netlify.app"
+
 
 // Detectar si es disposito movil
 export const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
