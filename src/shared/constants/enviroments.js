3d15a088ac35b97ab238635de5e4767e0689dc48
cpalomino1990 <89701110+cpalomino1990@users.noreply.github.com>
2025-04-29 11:43:26 -0500

    aa

--- src/shared/constants/enviroments.js
+++ src/shared/constants/enviroments.js
@@ -1,7 +1,6 @@
 // export const host = "http://192.168.1.151:5502";
 // export const host = "http://127.0.0.1:5502"; // Production
-export const host = "https://peaceful-biscotti-94162e.netlify.app"
-
+export const host = "https://merry-palmier-ba5f3e.netlify.app"; // Production
 
 // Detectar si es disposito movil
 export const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
